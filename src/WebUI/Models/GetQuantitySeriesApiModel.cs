f2308791cfa7ca63c342bdf00668d400e24f1c5a
Jan Sustr <jan.sustr11@gmail.com>
2020-11-02 22:16:51 +0100

    change model

--- src/WebUI/Models/GetQuantitySeriesApiModel.cs
+++ src/WebUI/Models/GetQuantitySeriesApiModel.cs
@@ -1,15 +1,17 @@
 using System;
 using Electricity.Application.Common.Enums;
+using Electricity.Application.Common.Mappings;
+using Electricity.Application.Series.Queries.GetQuantitySeries;
 
 namespace Electricity.WebUI.Models
 {
-    public class GetQuantitySeriesApiModel
+    public class GetQuantitySeriesApiModel : IMapFrom<GetQuantitySeriesQuery>
     {
         public Guid GroupId { get; set; }
         public byte Arch { get; set; }
         public string Qty { get; set; }
-        public DateTime RStart { get; set; }
-        public DateTime REnd { get; set; }
+        public DateTime? RStart { get; set; }
+        public DateTime? REnd { get; set; }
         public AggregationMethod AggMeth { get; set; }
         public int AggInt { get; set; }
     }
