1d087f50b79ff5243a353ad476fd7043641e95ab
Jan Sustr <jan.sustr11@gmail.com>
2021-02-01 11:33:59 +0100

    test: intergration test setup

--- tests/Applicaton.IntegrationTests/Testing.cs
+++ tests/Applicaton.IntegrationTests/Testing.cs
@@ -1,7 +1,66 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Electricity.Application.Common.Interfaces;
+using Electricity.WebUI;
+using MediatR;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
 using NUnit.Framework;
 
 [SetUpFixture]
 public class Testing
 {
+    private static IConfigurationRoot _configuration;
+    private static IServiceScopeFactory _scopeFactory;
 
+    private static Guid _currentUserId;
+
+    [OneTimeSetUp]
+    public void RunBeforeAnyTests()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .AddEnvironmentVariables();
+
+        _configuration = builder.Build();
+
+        var startup = new Startup(_configuration);
+
+        var services = new ServiceCollection();
+
+        services.AddSingleton(Mock.Of<IWebHostEnvironment>(w =>
+            w.EnvironmentName == "Development" &&
+            w.ApplicationName == "CleanArchitecture.WebUI"));
+
+        services.AddLogging();
+
+        startup.ConfigureServices(services);
+
+        // Replace service registration for ICurrentUserService
+        // Remove existing registration
+        var currentUserServiceDescriptor = services.FirstOrDefault(d =>
+            d.ServiceType == typeof(ICurrentUserService));
+
+        services.Remove(currentUserServiceDescriptor);
+
+        // Register testing version
+        services.AddTransient(provider =>
+            Mock.Of<ICurrentUserService>(s => s.UserId == _currentUserId));
+
+    }
+
+
+    public static async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request)
+    {
+        using var scope = _scopeFactory.CreateScope();
+
+        var mediator = scope.ServiceProvider.GetService<ISender>();
+
+        return await mediator.Send(request);
+    }
 }
