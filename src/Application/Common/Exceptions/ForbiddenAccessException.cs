2ae49ce60a1be86113ccb1486a0f9581598a694b
Jan Sustr <jan.sustr11@gmail.com>
2021-02-03 19:19:05 +0100

    feat: add authorization

---
+++ src/Application/Common/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Electricity.Application.Common.Exceptions
+{
+    public class ForbiddenAccessException : Exception
+    {
+        public ForbiddenAccessException() : base() { }
+    }
+}
