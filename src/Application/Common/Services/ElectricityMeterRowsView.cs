69e2bd2f6afb311f1a2b8e5db952d3504d7b7fc9
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-26 12:46:23 +0100

    feat: ElectricityMeterService

---
+++ src/Application/Common/Services/ElectricityMeterRowsView.cs
@@ -0,0 +1,60 @@
+﻿using DataSource;
+using Electricity.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Electricity.Application.Common.Services
+{
+    public class ElectricityMeterRowsView
+    {
+        private readonly ElectricityMeterQuantity[] _quantities;
+        private IEnumerable<Tuple<DateTime, float[]>> _rows;
+
+        public ElectricityMeterRowsView(
+            ElectricityMeterQuantity[] quantities,
+            IEnumerable<Tuple<DateTime, float[]>> rows)
+        {
+            _quantities = quantities;
+            _rows = rows;
+        }
+
+        public float GetDifference(ElectricityMeterQuantity quantity)
+        {
+            var i = GetIndexOfQuantity(quantity);
+            var firstRow = _rows.First();
+            var lastRow = _rows.Last();
+
+            return lastRow.Item2[i] - firstRow.Item2[i];
+        }
+
+        public float GetDifferencePerMonths(ElectricityMeterQuantity quantity)
+        {
+            throw new Exception("not implemented");
+        }
+
+        public Interval GetInterval()
+        {
+            var start = _rows.First().Item1;
+            var end = _rows.Last().Item1;
+            return new Interval(start, end);
+        }
+
+        public bool HasQuantity(ElectricityMeterQuantity quantity)
+        {
+            return _quantities.Contains(quantity);
+        }
+
+        private int GetIndexOfQuantity(ElectricityMeterQuantity quantity)
+        {
+            int i = Array.IndexOf(_quantities, quantity);
+            if (i == -1)
+            {
+                throw new Exception($"does not have {nameof(quantity)}");
+            }
+
+            return i;
+        }
+    }
+}
---
+++ src/Application/Common/Services/ElectricityMeterService.cs
@@ -0,0 +1,61 @@
+﻿using DataSource;
+using Electricity.Application.Common.Enums;
+using Electricity.Application.Common.Interfaces;
+using Electricity.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Electricity.Application.Common.Services
+{
+    public enum ElectricityMeterQuantity
+    {
+        ActiveEnergy,
+        ReactiveEnergyL,
+        ReactiveEnergyC,
+    }
+
+    public class ElectricityMeterService
+    {
+        private readonly ITableCollection _tableCollection;
+
+        public ElectricityMeterService(
+            ITableCollection tableCollection)
+        {
+            _tableCollection = tableCollection;
+        }
+
+        public ElectricityMeterRowsView GetRowsView(Guid groupId, Interval interval, ElectricityMeterQuantity[] quantities)
+        {
+            var table = _tableCollection.GetTable(groupId, (byte)Arch.ElectricityMeter);
+
+            var q = quantities.Select(q => GetQuantity(q)).ToArray();
+
+            var rows = table.GetRows(new Models.Queries.GetRowsQuery
+            {
+                Interval = interval,
+                Quantities = q
+            });
+
+            if (rows.Count() == 0)
+            {
+                return null;
+            }
+
+            return new ElectricityMeterRowsView(quantities, rows);
+        }
+
+        private Quantity GetQuantity(ElectricityMeterQuantity quantity)
+        {
+            switch (quantity)
+            {
+                case ElectricityMeterQuantity.ActiveEnergy:
+                    return new Quantity("3EP", "Wh");
+
+                default:
+                    throw new ArgumentException("invalid quantity");
+            }
+        }
+    }
+}
