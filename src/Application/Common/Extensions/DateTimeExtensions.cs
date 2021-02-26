aa9ab3a77a6376f041077857388e098c2bd0861c
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-26 18:32:09 +0100

    feat: datetime floor extension

---
+++ src/Application/Common/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electricity.Application.Common.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime FloorYear(this DateTime source)
+        {
+            var d = source.FloorMonth();
+            return source.AddMonths(-source.Month);
+        }
+
+        public static DateTime FloorMonth(this DateTime source)
+        {
+            var d = source.FloorDay();
+            return source.AddDays(-source.Day);
+        }
+
+        public static DateTime FloorDay(this DateTime source)
+        {
+            var d = source.FloorHour();
+            return d.AddHours(source.Hour);
+        }
+
+        public static DateTime FloorHour(this DateTime source)
+        {
+            var d = source.AddMilliseconds(-source.Millisecond);
+            return d.AddMinutes(-source.Minute);
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Common/Extensions/DateTimeExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using Electricity.Application.Common.Extensions;
+
+namespace Electricity.Application.UnitTests.Common.Extensions
+{
+    internal class DateTimeExtensionsTest
+    {
+        public class TestData
+        {
+            public DateTime Source { get; set; }
+            public DateTime ExpectedResult { get; set; }
+        }
+
+        private static TestData[] _testData = new[]{
+            new TestData(){
+                Source = new DateTime(2021,1,1,1,1,1,1), ExpectedResult=new DateTime(2021, 1, 0) }};
+
+        [Test]
+        public void FloorMonthTest([ValueSource("_testData")] TestData data)
+        {
+            data.Source.FloorMonth().Should().Be(data.ExpectedResult);
+        }
+    }
+}
