9c1dd4768e8b59a236aed5b87c25055897780cc4
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-26 17:33:10 +0100

    feat: power service

---
+++ src/Application/Common/Services/PowerRowsView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electricity.Application.Common.Services
+{
+    public class PowerRowsView
+    {
+        public PowerRowsView()
+        {
+        }
+    }
+}
---
+++ src/Application/Common/Services/PowerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electricity.Application.Common.Services
+{
+    public class PowerService
+    {
+        public PowerService()
+        {
+        }
+
+        public PowerRowsView GetRowsView()
+        {
+            throw new Exception("not implem");
+        }
+    }
+}
