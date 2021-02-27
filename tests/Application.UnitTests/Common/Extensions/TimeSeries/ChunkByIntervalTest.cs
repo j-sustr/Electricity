89e1ad2a53d8b875175b2f4753a600623541d710
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-27 08:28:16 +0100

    test

--- tests/Application.UnitTests/Common/Extensions/TimeSeries/ChunkByIntervalTest.cs
+++ tests/Application.UnitTests/Common/Extensions/TimeSeries/ChunkByIntervalTest.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Electricity.Application.UnitTests.Common.Extensions.TimeSeries
 {
@@ -19,21 +20,28 @@ namespace Electricity.Application.UnitTests.Common.Extensions.TimeSeries
             new TestData(){
                 Source = new VariableIntervalTimeSeries<int>(new Tuple<DateTime, int>[]
                 {
-                    Tuple.Create(new DateTime(2021, 1, 1), 1),
-                    Tuple.Create(new DateTime(2021, 1, 10), 1),
                     Tuple.Create(new DateTime(2021, 2, 1), 1),
-                    Tuple.Create(new DateTime(2021, 2, 15), 1),
+                    Tuple.Create(new DateTime(2021, 2, 10), 1),
+                    Tuple.Create(new DateTime(2021, 3, 1), 1),
+                    Tuple.Create(new DateTime(2021, 3, 15), 1),
+                    Tuple.Create(new DateTime(2021, 5, 20), 1),
+                    Tuple.Create(new DateTime(2021, 5, 23), 1),
                 }),
                 ExpectedResult = new VariableIntervalTimeSeries<int>[]
                 {
                     new VariableIntervalTimeSeries<int>(new Tuple<DateTime, int>[] {
-                        Tuple.Create(new DateTime(2021, 1, 1), 1),
-                        Tuple.Create(new DateTime(2021, 1, 10), 1),
+                        Tuple.Create(new DateTime(2021, 2, 1), 1),
+                        Tuple.Create(new DateTime(2021, 2, 10), 1),
                     }),
                     new VariableIntervalTimeSeries<int>(new Tuple<DateTime, int>[] {
-                        Tuple.Create(new DateTime(2021, 2, 1), 1),
-                        Tuple.Create(new DateTime(2021, 2, 15), 1),
-                    })
+                        Tuple.Create(new DateTime(2021, 3, 1), 1),
+                        Tuple.Create(new DateTime(2021, 3, 15), 1),
+                    }),
+                    null,
+                    new VariableIntervalTimeSeries<int>(new Tuple<DateTime, int>[] {
+                        Tuple.Create(new DateTime(2021, 5, 20), 1),
+                        Tuple.Create(new DateTime(2021, 5, 23), 1),
+                    }),
                 }
             },
         };
@@ -41,7 +49,22 @@ namespace Electricity.Application.UnitTests.Common.Extensions.TimeSeries
         [Test]
         public void ShouldChunkByMonth([ValueSource("_testData")] TestData data)
         {
-            data.Source.ChunkByMonth().Should().BeEquivalentTo(data.ExpectedResult);
+            var result = data.Source.ChunkByMonth();
+
+            result.Should().BeEquivalentTo(data.ExpectedResult);
+            var enumerator = data.ExpectedResult.GetEnumerator();
+            foreach (var series in result)
+            {
+                enumerator.MoveNext();
+
+                if (enumerator.Current == null)
+                {
+                    series.Should().BeNull();
+                    continue;
+                }
+
+                series.Entries().Should().BeEquivalentTo(enumerator.Current.Entries());
+            }
         }
     }
 }
