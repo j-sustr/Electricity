39265b5eec091aacaf04391634d36c768bd4cc16
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-27 06:50:11 +0100

    feat

--- src/Application/Common/Services/PowerRowsView.cs
+++ src/Application/Common/Services/PowerRowsView.cs
@@ -9,5 +9,10 @@ namespace Electricity.Application.Common.Services
         public PowerRowsView()
         {
         }
+
+        public float GetPeakDemandInMonths()
+        {
+            throw new Exception("not impl");
+        }
     }
 }
