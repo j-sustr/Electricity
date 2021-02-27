a8f49ca27d62f4dde6f4332537438d8478c0e4c5
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-27 11:20:15 +0100

    feat: add validator

---
+++ src/Application/Costs/Queries/GetCostsOverviewQuery/GetCostsOverviewQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electricity.Application.Costs.Queries.GetCostsOverview
+{
+    public class GetCostsOverviewQueryValidator : AbstractValidator<GetCostsOverviewQuery>
+    {
+        public GetCostsOverviewQueryValidator()
+        {
+            RuleFor(x => x.Interval1)
+               .NotNull()
+               .WithMessage("Interval1 is required.");
+        }
+    }
+}
