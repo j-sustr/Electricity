8e036648da4a1c0f94d3ff257e98e0330fae7dbc
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-28 12:16:11 +0100

    feat: costs controller

---
+++ src/WebUI/Controllers/CostsController.cs
@@ -0,0 +1,22 @@
+﻿using Electricity.Application.Costs.Queries.GetCostsDetail;
+using Electricity.Application.Costs.Queries.GetCostsOverview;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Electricity.WebUI.Controllers
+{
+    public class CostsController : ApiController
+    {
+        [HttpGet("overview")]
+        public async Task<ActionResult<CostsOverviewDto>> GetOverviewAsync([FromQuery] GetCostsOverviewQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+
+        [HttpGet("detail")]
+        public async Task<ActionResult<CostsDetailDto>> GetDetailAsync([FromQuery] GetCostsDetailQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
