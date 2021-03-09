d15694bbb940552a765bfb52589f07faa82ac49b
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-03-09 11:40:59 +0100

    enable AutoAuthorizeMiddleware in production

--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -93,9 +93,10 @@ namespace Electricity.WebUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<AutoAuthorizeMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseMiddleware<AutoAuthorizeMiddleware>();
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
                 app.UseCors(builder =>
