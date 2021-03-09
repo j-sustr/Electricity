72a312f6969fdfd40f411649d90dff86f8ff9b0e
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-03-09 15:41:48 +0100

    cleanup

--- src/Application/Series/Queries/GetQuantitySeries/QuantitySeriesDto.cs
+++ src/Application/Series/Queries/GetQuantitySeries/QuantitySeriesDto.cs
@@ -6,7 +6,6 @@ namespace Electricity.Application.Series.Queries.GetQuantitySeries
 {
     public class QuantitySeriesDto
     {
-        public Interval Interval { get; set; }
         public IList<Tuple<DateTime, float>> Entries { get; set; }
     }
 }
