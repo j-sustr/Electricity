54dd1acf770e470e8f1b04a3f9885847fff89caf
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-03-10 17:47:57 +0100

    feat: ceil interval

---
+++ src/Application/Common/Extensions/DateTime/CeilInterval.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electricity.Application.Common.Extensions
+{
+    public static partial class DateTimeExtensions
+    {
+        public static DateTime CeilYear(this DateTime source)
+        {
+            var d = source.FloorMonth();
+            d = d.AddMonths(-d.Month + 1);
+            return d.AddYears(1);
+        }
+
+        public static DateTime CeilMonth(this DateTime source)
+        {
+            var d = source.FloorDay();
+            d = d.AddDays(-d.Day + 1);
+            return d.AddMonths(1);
+        }
+
+        public static DateTime CeilDay(this DateTime source)
+        {
+            var d = source.FloorHour();
+            d = d.AddHours(-d.Hour);
+            return d.AddDays(1);
+        }
+
+        public static DateTime CeilHour(this DateTime source)
+        {
+            var d = source.AddMilliseconds(-source.Millisecond);
+            d = d.AddSeconds(-source.Second);
+            d = d.AddMinutes(-source.Minute);
+            d = d.AddHours(1);
+            return d;
+        }
+    }
+}
---
+++ tests/Application.UnitTests/Common/Extensions/DateTime/CeilIntervalTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using Electricity.Application.Common.Extensions;
+using FluentAssertions;
+
+namespace Electricity.Application.UnitTests.Common.Extensions
+{
+    public class CeilIntervalTest
+    {
+        public class TestData
+        {
+            public DateTime Source { get; set; }
+            public DateTime ExpectedResult { get; set; }
+        }
+
+        private static TestData[] _yearTestData = new[]{
+            new TestData(){
+                Source = new DateTime(2021, 1, 10, 20, 30, 40, 50),
+                ExpectedResult=new DateTime(2022, 1, 1)
+            }
+        };
+
+        [Test]
+        public void ShouldCeilYear([ValueSource("_yearTestData")] TestData data)
+        {
+            data.Source.CeilYear().Should().Be(data.ExpectedResult);
+        }
+
+        private static TestData[] _monthTestData = new[]{
+            new TestData(){
+                Source = new DateTime(2021, 3, 10, 20, 30, 40, 50),
+                ExpectedResult=new DateTime(2021, 4, 1)
+            }
+        };
+
+        [Test]
+        public void ShouldCeilMonth([ValueSource("_monthTestData")] TestData data)
+        {
+            data.Source.CeilMonth().Should().Be(data.ExpectedResult);
+        }
+
+        private static TestData[] _dayTestData = new[]{
+            new TestData(){
+                Source = new DateTime(2021,1,2,3,4,5,6),
+                ExpectedResult=new DateTime(2021, 1, 3, 0, 0, 0)
+            }
+        };
+
+        [Test]
+        public void ShouldCeilDay([ValueSource("_dayTestData")] TestData data)
+        {
+            data.Source.CeilDay().Should().Be(data.ExpectedResult);
+        }
+    }
+}
