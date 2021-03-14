1a8c06417564d471b8e5c8b4772d08c1e0b38205
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-03-14 18:40:50 +0100

    impl: peak demand

---
+++ src/Application/PeakDemand/Queries/GetPeakDemandOverview/GetPeakDemandOverviewQuery.cs
@@ -0,0 +1,25 @@
+﻿using Electricity.Application.Common.Models.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Electricity.Application.PeakDemand.Queries.GetPeakDemandOverview
+{
+    public class GetPeakDemandOverviewQuery : IRequest<PeakDemandOverviewDto>
+    {
+        public IntervalDto Interval1 { get; set; }
+
+        public IntervalDto? Interval2 { get; set; }
+    }
+
+    public class GetCostsDetailQueryHandler : IRequestHandler<GetPeakDemandOverviewQuery, PeakDemandOverviewDto>
+    {
+        public Task<PeakDemandOverviewDto> Handle(GetPeakDemandOverviewQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Application/PeakDemand/Queries/GetPeakDemandOverview/PeakDemandOverviewDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Electricity.Application.PeakDemand.Queries.GetPeakDemandOverview
+{
+    public class PeakDemandOverviewDto
+    {
+    }
+}
