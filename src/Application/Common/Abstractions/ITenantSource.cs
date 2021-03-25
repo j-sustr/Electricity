b7c356df51af93e7a2a42cc72281cd76c5d76078
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-03-25 13:09:15 +0100

    cleanup

--- src/Application/Common/Abstractions/ITenantSource.cs
+++
@@ -1,9 +0,0 @@
-using Electricity.Application.Common.Models;
-
-namespace Electricity.Application.Common.Interfaces
-{
-    public interface ITenantSource
-    {
-        Tenant[] ListTenants();
-    }
-}
