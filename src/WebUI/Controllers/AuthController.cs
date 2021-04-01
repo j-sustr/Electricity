7aab2d7d8346c5bf94d0506b99151283e397c2ab
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-01 19:55:34 +0200

    impl auth

---
+++ src/WebUI/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Electricity.WebUI.Controllers
+{
+    public class AuthController : ApiController
+    {
+        private readonly Application.Common.Interfaces.IAuthenticationService _authService;
+
+        public AuthController(Application.Common.Interfaces.IAuthenticationService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(string username, string password)
+        {
+            var guid = _authService.Login(username, password);
+            if (guid == Guid.Empty)
+            {
+                return BadRequest();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim("username", username),
+                new Claim(ClaimTypes.NameIdentifier, username),
+                new Claim(ClaimTypes.Role, "User"),
+                new Claim("id", guid.ToString()),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                //AllowRefresh = <bool>,
+                // Refreshing the authentication session should be allowed.
+
+                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                // The time at which the authentication ticket expires. A
+                // value set here overrides the ExpireTimeSpan option of
+                // CookieAuthenticationOptions set with AddCookie.
+
+                //IsPersistent = true,
+                // Whether the authentication session is persisted across
+                // multiple requests. When used with cookies, controls
+                // whether the cookie's lifetime is absolute (matching the
+                // lifetime of the authentication ticket) or session-based.
+            };
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<ActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return Ok();
+        }
+    }
+}
--- src/WebUI/Controllers/UserController.cs
+++ src/WebUI/Controllers/UserController.cs
@@ -16,21 +16,23 @@ namespace Electricity.WebUI.Controllers
 {
     public class UserController : ApiController
     {
-        private readonly Application.Common.Interfaces.IAuthenticationService _authService;
+        private readonly ITenantProvider _tenantProvider;
 
-        private readonly ILogger<UserController> _logger;
-
-        public UserController(
-            Application.Common.Interfaces.IAuthenticationService authService,
-            ILogger<UserController> logger)
+        public UserController(ITenantProvider tenantProvider)
         {
-            _authService = authService;
-            _logger = logger;
+            _tenantProvider = tenantProvider;
         }
 
         [HttpGet("current-user")]
-        public ActionResult<UserDto> GetCurrentUser()
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
+            var tenant = _tenantProvider.GetTenant();
+            if (tenant == null)
+            {
+                await HttpContext.SignOutAsync();
+                return Ok(null);
+            }
+
             var user = HttpContext.User;
             if (user == null)
             {
@@ -41,60 +43,6 @@ namespace Electricity.WebUI.Controllers
             {
                 Username = user.FindFirstValue("username")
             });
-        }
-
-        [HttpPost("login")]
-        public async Task<ActionResult> Login(string username, string password)
-        {
-            var guid = _authService.Login(username, password);
-            if (guid == Guid.Empty)
-            {
-                return BadRequest();
-            }
-
-            var claims = new List<Claim>
-            {
-                new Claim("username", username),
-                new Claim(ClaimTypes.NameIdentifier, username),
-                new Claim(ClaimTypes.Role, "User"),
-                new Claim("id", guid.ToString()),
-            };
-
-            var claimsIdentity = new ClaimsIdentity(
-                claims, CookieAuthenticationDefaults.AuthenticationScheme);
-
-            var authProperties = new AuthenticationProperties
-            {
-                //AllowRefresh = <bool>,
-                // Refreshing the authentication session should be allowed.
-
-                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
-                // The time at which the authentication ticket expires. A
-                // value set here overrides the ExpireTimeSpan option of
-                // CookieAuthenticationOptions set with AddCookie.
-
-                //IsPersistent = true,
-                // Whether the authentication session is persisted across
-                // multiple requests. When used with cookies, controls
-                // whether the cookie's lifetime is absolute (matching the
-                // lifetime of the authentication ticket) or session-based.
-            };
-
-            await HttpContext.SignInAsync(
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                new ClaimsPrincipal(claimsIdentity),
-                authProperties);
-
-            return Ok();
-        }
-
-        [Authorize]
-        [HttpPost("logout")]
-        public async Task<ActionResult> Logout()
-        {
-            await HttpContext.SignOutAsync();
-
-            return Ok();
         }
     }
 }
