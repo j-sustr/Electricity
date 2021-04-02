c492446d3a6df2aedca5497c569a0035dce1d91a
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-02 12:31:34 +0200

    fix user is authenticated resolution

--- src/WebUI/Controllers/UserController.cs
+++ src/WebUI/Controllers/UserController.cs
@@ -1,15 +1,9 @@
 ﻿using Electricity.Application.Common.Interfaces;
 using Electricity.Application.Common.Models.Dtos;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using NJsonSchema.Annotations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -36,7 +30,7 @@ namespace Electricity.WebUI.Controllers
             }
 
             var user = HttpContext.User;
-            if (user == null)
+            if (user?.Identity.IsAuthenticated != true)
             {
                 return null;
             }
