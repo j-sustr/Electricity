26327a842d4d6e4c4bf16c6343a12c9ae018e37b
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-03 10:39:31 +0200

    add Resources.dll

--- scripts/copy-dlls.js
+++ scripts/copy-dlls.js
@@ -2,6 +2,7 @@ import * as path from 'https://deno.land/std/path/mod.ts';
 
 const dllSrcPaths = [
     String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\DataSource.dll`,
+    String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\Resources.dll`,
     String.raw`C:\__work__\kmb\210322-net5\FileDataSource\bin\Release\net5.0\FileDataSource.dll`,
     String.raw`C:\__work__\kmb\210322-net5\DBDataSource\bin\Release\net5.0\DBDataSource.dll`,
 ];
