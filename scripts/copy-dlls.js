dcb1d12f47f72cb4a2ff01d66a38e1ffd0c5beb8
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-05 09:55:27 +0200

    update script

--- scripts/copy-dlls.js
+++ scripts/copy-dlls.js
@@ -1,16 +1,25 @@
 import * as path from 'https://deno.land/std/path/mod.ts';
 
-const dllSrcPaths = [
+const dllPaths = [
     String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\DataSource.dll`,
     String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\Resources.dll`,
     String.raw`C:\__work__\kmb\210322-net5\FileDataSource\bin\Release\net5.0\FileDataSource.dll`,
     String.raw`C:\__work__\kmb\210322-net5\DBDataSource\bin\Release\net5.0\DBDataSource.dll`,
 ];
 
+const pdbPaths = [
+    String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\DataSource.pdb`,
+    String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\Resources.pdb`,
+    String.raw`C:\__work__\kmb\210322-net5\FileDataSource\bin\Release\net5.0\FileDataSource.pdb`,
+    String.raw`C:\__work__\kmb\210322-net5\DBDataSource\bin\Release\net5.0\DBDataSource.pdb`,
+];
+
+const srcPaths = [...dllPaths, ...pdbPaths];
+
 const destDir = String.raw`C:\__work__\dotnet\Electricity\dll`;
 
-for (const dllPath of dllSrcPaths) {
-    const destPath = path.join(destDir, path.basename(dllPath));
-    console.log(`Copying "${dllPath}" to "${destPath}"`);
-    Deno.copyFileSync(dllPath, destPath);
+for (const srcPath of srcPaths) {
+    const destPath = path.join(destDir, path.basename(srcPath));
+    console.log(`Copying "${srcPath}" to "${destPath}"`);
+    Deno.copyFileSync(srcPath, destPath);
 }
