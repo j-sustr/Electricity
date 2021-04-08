65b094877b0de395c2f8f1786a2fc4a3118215ae
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-08 08:24:31 +0200

    update kmb dlls

--- scripts/copy-dlls.js
+++ scripts/copy-dlls.js
@@ -1,17 +1,17 @@
 import * as path from 'https://deno.land/std/path/mod.ts';
 
 const dllPaths = [
-    String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\DataSource.dll`,
-    String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\Resources.dll`,
-    String.raw`C:\__work__\kmb\210322-net5\FileDataSource\bin\Release\net5.0\FileDataSource.dll`,
-    String.raw`C:\__work__\kmb\210322-net5\DBDataSource\bin\Release\net5.0\DBDataSource.dll`,
+    String.raw`C:\__work__\kmb\datasource-net5\DataSource\bin\Release\net5.0\DataSource.dll`,
+    String.raw`C:\__work__\kmb\datasource-net5\DataSource\bin\Release\net5.0\Resources.dll`,
+    String.raw`C:\__work__\kmb\datasource-net5\FileDataSource\bin\Release\net5.0\FileDataSource.dll`,
+    String.raw`C:\__work__\kmb\datasource-net5\DBDataSource\bin\Release\net5.0\DBDataSource.dll`,
 ];
 
 const pdbPaths = [
-    String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\DataSource.pdb`,
-    String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\Resources.pdb`,
-    String.raw`C:\__work__\kmb\210322-net5\FileDataSource\bin\Release\net5.0\FileDataSource.pdb`,
-    String.raw`C:\__work__\kmb\210322-net5\DBDataSource\bin\Release\net5.0\DBDataSource.pdb`,
+    String.raw`C:\__work__\kmb\datasource-net5\DataSource\bin\Release\net5.0\DataSource.pdb`,
+    String.raw`C:\__work__\kmb\datasource-net5\DataSource\bin\Release\net5.0\Resources.pdb`,
+    String.raw`C:\__work__\kmb\datasource-net5\FileDataSource\bin\Release\net5.0\FileDataSource.pdb`,
+    String.raw`C:\__work__\kmb\datasource-net5\DBDataSource\bin\Release\net5.0\DBDataSource.pdb`,
 ];
 
 const srcPaths = [...dllPaths, ...pdbPaths];
