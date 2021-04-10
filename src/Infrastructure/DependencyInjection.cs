92319ee21e1e21e2741a01ddb0a87b0dde012406
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-10 08:23:54 +0200

    cleanup

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -24,7 +24,6 @@ namespace Electricity.Infrastructure
 
             services.AddScoped<ApplicationDataSource>(); // DEBUG
             services.AddScoped<IGroupRepository, ApplicationDataSource>();
-            services.AddScoped<IQuantityService, ApplicationDataSource>();
             services.AddScoped<IArchiveRepository, ApplicationDataSource>();
             services.AddScoped<IAuthenticationService, ApplicationDataSource>();
 
