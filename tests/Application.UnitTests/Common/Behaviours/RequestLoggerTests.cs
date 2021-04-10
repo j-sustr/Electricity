c4ec70d56e4036777b5c78efe5b0a65835214dfa
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-10 09:15:01 +0200

    cleanup

--- tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
+++ tests/Application.UnitTests/Common/Behaviours/RequestLoggerTests.cs
@@ -1,25 +1,16 @@
-﻿using Electricity.Application.Common.Behaviours;
-using Electricity.Application.Common.Interfaces;
-using Microsoft.Extensions.Logging;
+﻿using Electricity.Application.Common.Interfaces;
 using Moq;
-using NUnit.Framework;
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Electricity.Application.UnitTests.Common.Behaviours
 {
     public class RequestLoggerTests
     {
         private readonly Mock<ICurrentUserService> _currentUserService;
-        private readonly Mock<IIdentityService> _identityService;
 
 
         public RequestLoggerTests()
         {
             _currentUserService = new Mock<ICurrentUserService>();
-
-            _identityService = new Mock<IIdentityService>();
         }
     }
 }
