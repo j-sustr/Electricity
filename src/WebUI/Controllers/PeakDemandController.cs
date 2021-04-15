4f5e13d932caa251951a72edc85089f137cf126b
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-15 11:39:29 +0200

    impl PeakDemandController

---
+++ src/WebUI/Controllers/PeakDemandController.cs
@@ -0,0 +1,22 @@
+﻿using Electricity.Application.PeakDemand.Queries.GetPeakDemandDetail;
+using Electricity.Application.PeakDemand.Queries.GetPeakDemandOverview;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Electricity.WebUI.Controllers
+{
+    public class PeakDemandController : ApiController
+    {
+        [HttpGet("overview")]
+        public async Task<ActionResult<PeakDemandOverviewDto>> GetOverviewAsync([FromQuery] GetPeakDemandOverviewQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+
+        [HttpGet("detail")]
+        public async Task<ActionResult<PeakDemandDetailDto>> GetDetailAsync([FromQuery] GetPeakDemandDetailQuery query)
+        {
+            return await Mediator.Send(query);
+        }
+    }
+}
