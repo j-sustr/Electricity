6d48603cba4334f57cf2ad8d3e48a697965ead50
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-21 23:56:23 +0200

    cleanup dead code

--- src/WebUI/Mappings/MappingProfile.cs
+++ src/WebUI/Mappings/MappingProfile.cs
@@ -1,9 +1,6 @@
 ﻿using AutoMapper;
 using KMB.DataSource;
-using Electricity.Application.Common.Extensions;
-using Electricity.WebUI.Models;
 using Electricity.Application.Common.Models.Dtos;
-using Electricity.Application.Archive.Queries.GetQuantitySeries;
 
 namespace Electricity.WebUI.Mappings
 {
@@ -12,12 +9,6 @@ namespace Electricity.WebUI.Mappings
         public MappingProfile()
         {
             CreateMap<SmpMeasNameDB, SmpMeasNameDBDto>();
-
-            CreateMap<string, Quantity>()
-                .ConvertUsing(s => QuantityExtensions.FromString(s));
-
-            CreateMap<GetQuantitySeriesApiModel, GetQuantitySeriesQuery>()
-                .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Qty));
         }
     }
 }
