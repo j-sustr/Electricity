34f8e777f79ab0821df81a802652e141bdd2dc7c
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-22 08:57:56 +0200

    refactor

--- src/Application/Common/Services/ArchiveRepositoryService.cs
+++ src/Application/Common/Services/ArchiveRepositoryService.cs
@@ -1,11 +1,34 @@
-﻿using Electricity.Application.Common.Interfaces;
+﻿using Electricity.Application.Common.Enums;
+using Electricity.Application.Common.Interfaces;
 using Electricity.Application.Common.Models;
+using KMB.DataSource;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Electricity.Application.Common.Services
 {
+    public abstract class GetRowsViewQueryBase
+    {
+        public Guid GroupId { get; set; }
+        public Interval Range { get; set; } = Interval.Unbounded;
+        public uint Aggregation { get; set; } = 0;
+        public EEnergyAggType EnergyAggType { get; set; } = EEnergyAggType.Cumulative;
+    }
+
+    public class GetPowerRowsViewQuery : GetRowsViewQueryBase
+    {
+        public PowerQuantity[] Quantities { get; set; }
+    }
+
+    public class GetElectricityMeterRowsViewQuery : GetRowsViewQueryBase
+    {
+        public ElectricityMeterQuantity[] Quantities { get; set; }
+    }
+
+    
+
     public class ArchiveRepositoryService
     {
         protected readonly IArchiveRepository _archiveRepository;
@@ -17,6 +40,50 @@ namespace Electricity.Application.Common.Services
         {
             _archiveRepository = archiveRepository;
             _groupRepository = groupRepository;
+        }
+
+        public ElectricityMeterRowsView GetElectricityMeterRowsView(GetElectricityMeterRowsViewQuery query)
+        {
+            var archive = _archiveRepository.GetArchive(query.GroupId, (byte)Arch.ElectricityMeter);
+
+            var quantities = query.Quantities.Select(q => q.ToQuantity()).ToArray();
+
+            var rows = archive.GetRows(new GetArchiveRowsQuery
+            {
+                Range = query.Range,
+                Quantities = quantities,
+                Aggregation = query.Aggregation,
+                EnergyAggType = query.EnergyAggType
+            });
+
+            if (rows.Count() == 0)
+            {
+                return null;
+            }
+
+            return new ElectricityMeterRowsView(query.Quantities, rows);
+        }
+
+        public PowerRowsView GetPowerRowsView(GetPowerRowsViewQuery query)
+        {
+            var archive = _archiveRepository.GetArchive(query.GroupId, (byte)Arch.Main);
+
+            var quantities = query.Quantities.Select(q => q.ToQuantity()).ToArray();
+
+            var rows = archive.GetRows(new GetArchiveRowsQuery
+            {
+                Range = query.Range,
+                Quantities = quantities,
+                Aggregation = query.Aggregation,
+                EnergyAggType = query.EnergyAggType
+            });
+
+            if (rows.Count() == 0)
+            {
+                return null;
+            }
+
+            return new PowerRowsView(query.Quantities, rows);
         }
 
         public bool HasInterval(Interval interval, byte arch)
