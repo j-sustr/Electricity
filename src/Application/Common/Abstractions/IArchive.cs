5e006fa66e8b8feb599d68e632ffaae88f86f3f7
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-22 08:56:01 +0200

    move model

--- src/Application/Common/Abstractions/IArchive.cs
+++ src/Application/Common/Abstractions/IArchive.cs
@@ -1,11 +1,21 @@
 using System;
 using System.Collections.Generic;
 using Electricity.Application.Common.Models;
-using Electricity.Application.Common.Models.Queries;
 using KMB.DataSource;
 
 namespace Electricity.Application.Common.Interfaces
 {
+    public class GetArchiveRowsQuery
+    {
+        public Interval Range { get; set; } = Interval.Unbounded;
+
+        public Quantity[] Quantities { get; set; }
+
+        public uint Aggregation { get; set; } = 0;
+
+        public EEnergyAggType EnergyAggType { get; set; } = EEnergyAggType.Cumulative;
+    }
+
     public interface IArchive
     {
         Quantity[] GetQuantities(DateRange range);
