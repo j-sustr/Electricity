6cb31478a7e2dd2f2259770639f654dc5b495167
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-25 17:42:00 +0200

    impl

---
+++ src/Infrastructure/DataSource/DataSourceExtractor.cs
@@ -0,0 +1,36 @@
+﻿using Electricity.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electricity.Infrastructure.DataSource
+{
+    public class DataSourceExtractor
+    {
+        private readonly IArchiveRepository _archiveRepo;
+
+        public DataSourceExtractor(IArchiveRepository archiveRepo)
+        {
+            _archiveRepo = archiveRepo;
+        }
+        public void ExtractGroup(string groupId)
+        {
+            var guid = ParseGuidFromString(groupId, nameof(groupId));
+
+            var mainArch = _archiveRepo.GetArchive(guid, 0);
+
+            // mainArch.GetRows();
+        }
+
+        private Guid ParseGuidFromString(string id, string name)
+        {
+            if (!Guid.TryParse(id, out var guid))
+            {
+                throw new ArgumentException($"could not parse Guid", name);
+            }
+            return guid;
+        }
+    }
+}
