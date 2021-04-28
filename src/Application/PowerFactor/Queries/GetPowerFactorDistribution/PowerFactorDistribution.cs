4ed99680d3d1a952f82db42e4e17f88b24323231
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-28 14:15:13 +0200

    delete dead code

--- src/Application/PowerFactor/Queries/GetPowerFactorDistribution/PowerFactorDistribution.cs
+++ src/Application/PowerFactor/Queries/GetPowerFactorDistribution/PowerFactorDistribution.cs
@@ -26,37 +26,6 @@ namespace Electricity.Application.PowerFactor.Queries.GetPowerFactorDistribution
             return quanities.ToArray();
         }
 
-        public static float?[] CalcCosFiForPhase(ElectricityMeterRowsView emView, Phase phase)
-        {
-            var activeEnergy = emView.GetDifference(new ElectricityMeterQuantity
-            {
-                Type = ElectricityMeterQuantityType.ActiveEnergy,
-                Phase = phase,
-            });
-            var reactiveEnergyL = emView.GetDifference(new ElectricityMeterQuantity
-            {
-                Type = ElectricityMeterQuantityType.ReactiveEnergyL,
-                Phase = phase,
-            });
-
-            var ep = activeEnergy.Values().ToArray();
-            var eqL = reactiveEnergyL.Values().ToArray();
-            var cosFi = new float?[ep.Length];
-
-            for (int i = 0; i < ep.Length; i++)
-            {
-                if (ep[i] == 0 && eqL[i] == 0)
-                {
-                    cosFi[i] = null;
-                    continue;
-                }
-
-                cosFi[i] = ElectricityUtil.CalcCosFi(ep[i], eqL[i]);
-            }
-
-            return cosFi;
-        }
-
         public static int[] BinValues(float[] values, float[] thresholds)
         {
             int n = thresholds.Length + 1;
@@ -125,50 +94,6 @@ namespace Electricity.Application.PowerFactor.Queries.GetPowerFactorDistribution
         {
             const float RES = 0.001f;
             return start.ToString("0.000") + "-" + (end - RES).ToString("0.000");
-        }
-
-
-        public static Dictionary<string, int> CalcGeneralDistribution(float[] cosFi)
-        {
-            var counter = new Dictionary<string, int>();
-            counter.Add("0.950-1.000", 0);
-            counter.Add("0.900-0.949", 0);
-            counter.Add("0.800-0.899", 0);
-            counter.Add("0.700-0.799", 0);
-            counter.Add("0.600-0.699", 0);
-            counter.Add("0.000-0.599", 0);
-            counter.Add("outlier", 0);
-
-            foreach (float value in cosFi)
-            {
-                float absVal = Math.Abs(value);
-                switch (absVal)
-                {
-                    case var v when v <= 1 && v >= 0.95:
-                        counter["0.950-1.000"] += 1;
-                        break;
-                    case var v when v >= 0.9:
-                        counter["0.900-0.949"] += 1;
-                        break;
-                    case var v when v >= 0.8:
-                        counter["0.800-0.899"] += 1;
-                        break;
-                    case var v when v >= 0.7:
-                        counter["0.700-0.799"] += 1;
-                        break;
-                    case var v when v >= 0.6:
-                        counter["0.600-0.699"] += 1;
-                        break;
-                    case var v when v >= 0:
-                        counter["0.000-0.599"] += 1;
-                        break;
-                    default:
-                        counter["outlier"] += 1;
-                        break;
-                }
-            }
-
-            return counter;
         }
     }
 }
