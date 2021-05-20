c43391f2ac9b97094230856bb9d87860a000292f
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-05-20 22:48:43 +0200

    del script

--- scripts/check-dlls.js
+++
@@ -1,8 +0,0 @@
-const dllPath1 = String.raw`C:\__work__\kmb\210322-net5\DataSource\bin\Release\net5.0\DataSource.dll`;
-const dllPath2 = String.raw`C:\__work__\kmb\210322-net5\FileDataSource\bin\Release\net5.0\DataSource.dll`;
-
-const bytes1 = Deno.readFileSync(dllPath1);
-const bytes2 = Deno.readFileSync(dllPath2);
-
-console.log(bytes1);
-console.log(bytes2);
